ffe30ee6573bb43e9bbb432c9faffb758878c7b7
Harsh Srivastava <107634137+HK-Srivastava@users.noreply.github.com>
2022-12-22 16:44:13 +0530

    Create roman-numeral-convertor-looping.js

---
+++ roman-numeral-convertor-looping.js
@@ -0,0 +1,11 @@
+function convertToRoman(num) {
+    let map = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
+    let romanNumeral = '';
+    for(let key in map){  	      
+      while(num >= map[key]){
+        romanNumeral += key;
+        num -= map[key];
+      }
+    }
+    return romanNumeral;
+  };
